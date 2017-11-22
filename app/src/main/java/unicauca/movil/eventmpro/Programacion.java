9e28730e6f33affe866ec00d2dfb2760e53c15d2
rmarin17 <rimaca1107@gmail.com>
2017-11-22 10:41:46 -0500

    1.0

---
+++ app/src/main/java/unicauca/movil/eventmpro/Programacion.java
@@ -0,0 +1,18 @@
+package unicauca.movil.eventmpro;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import unicauca.movil.eventmpro.databinding.ActivityPonentesBinding;
+
+public class Programacion extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programacion);
+    }
+}
---
+++ app/src/main/java/unicauca/movil/eventmpro/adapters/ProgramacionAdapter.java
@@ -0,0 +1,38 @@
+package unicauca.movil.eventmpro.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by RicardoM on 22/11/2017.
+ */
+
+public class ProgramacionAdapter extends FragmentStatePagerAdapter {
+
+    List<Fragment> data;
+
+    public ProgramacionAdapter(FragmentManager fm, List<Fragment> data) {
+        super(fm);
+        this.data = data;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        int dia = position+1;
+        return "Dia "+dia;
+    }
+
+}
